a30fc24d86cf0e742b5a96999d9a89ed3940b3cc
Your Name <you@example.com>
2024-09-22 21:38:44 +0300

    create custom note item

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -5,6 +5,34 @@ class NotesViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return NoteItem();
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Card(
+        child: ListTile(
+          title: const Text(
+            'Note 1',
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+          subtitle: const Text(
+            'Note 1 description Note 1 descriptionNote 1 descriptionNote 1 descriptionNote 1 descriptionNote 1 descriptionNote 1 description',
+            style: TextStyle(fontSize: 20),
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: () {},
+          ),
+          onTap: () {},
+        ),
+      ),
+    );
   }
 }
